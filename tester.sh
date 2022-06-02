8f5bbd6e7a10d7081c014ba8483f2f14fcdecce5
Anthony Badea <badeaa3@gmail.com>
2022-06-02 10:38:27 +0200

    Update tester to remove pcharvard file path for train

--- tester.sh
+++ tester.sh
@@ -1,7 +1,7 @@
 # Example for launching training and evaluation
 
 # test training
-python -u /home/abadea/analysis/Pheno/cannonball-rpv-stops/run/train.py -a colalola -i /eos/home-a/abadea/data/rpv_multijet/madgraph/MG5_aMC_v2_7_3/pp_t1t1j_msu3_scan/npz/ptsmear_with_pz_recalculate_with_pt_sampleTillPositive_with_e_recalculate_at_fixed_parton_mass/0.0/shuf1/run_10_1389132_ptsmear0.00_shuf1.npz -o weights.npz -e 1 -b 10000 -j 4 -ns 2 2 -ni 1 -nc 30 -hd 200 200 200 -epe 1e-6 -epwe 1e-6 -epl 5 -epp 30
+python -u run/train.py -a colalola -i /eos/home-a/abadea/data/rpv_multijet/madgraph/MG5_aMC_v2_7_3/pp_t1t1j_msu3_scan/npz/ptsmear_with_pz_recalculate_with_pt_sampleTillPositive_with_e_recalculate_at_fixed_parton_mass/0.0/shuf1/run_10_1389132_ptsmear0.00_shuf1.npz -o weights.npz -e 1 -b 10000 -j 4 -ns 2 2 -ni 1 -nc 30 -hd 200 200 200 -epe 1e-6 -epwe 1e-6 -epl 5 -epp 30
 
 # test model evaluation
 python run/evaluate.py -d cpu -b 40000 -mp 1 -nc 30 -hd 200 200 200 -i /eos/home-a/abadea/data/rpv_multijet/madgraph/MG5_aMC_v2_7_3/pp_t1t1j_msu3_scan/npz/ptsmear_with_pz_recalculate_with_pt_sampleTillPositive_with_e_recalculate_at_fixed_parton_mass/0.0/shuf1/run_10_1389132_ptsmear0.00_shuf1.npz -o ./ -w /eos/home-a/abadea/data/rpv_multijet/training/model_final/ptsmear0.0/weights.npz
